c2b7748d35eda9ab21ef662f0ea9948a27050c2a
MaxanceV <maxance.villame@gmail.com>
2025-02-18 17:13:48 +0100

    post-list init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 
+import { provideHttpClient } from '@angular/common/http';
+import { PostService } from './services/post.service';
+
+
+import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+
+const routes: Routes = [
+  {path: '', component: PostListComponent}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
